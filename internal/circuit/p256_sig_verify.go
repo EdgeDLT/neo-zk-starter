de01858d2784e52bff6957887af5f583aa43a8a6
edgedlt <edgedlt@protonmail.com>
2024-10-15 08:07:20 +0100

    split out p256 verify function

--- internal/circuit/p256_sig_verify.go
+++ internal/circuit/p256_sig_verify.go
@@ -4,7 +4,6 @@ import (
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/std/algebra/emulated/sw_emulated"
 	"github.com/consensys/gnark/std/math/emulated"
-	"github.com/consensys/gnark/std/math/emulated/emparams"
 	"github.com/consensys/gnark/std/signature/ecdsa"
 	"github.com/nspcc-dev/neo-go/pkg/crypto/hash"
 	"github.com/nspcc-dev/neo-go/pkg/crypto/keys"
@@ -12,14 +11,18 @@ import (
 )
 
 type P256SigVerifyCircuit struct {
-	PublicKey   ecdsa.PublicKey[emparams.P256Fp, emparams.P256Fr] `gnark:",public"`
-	Signature   ecdsa.Signature[emparams.P256Fr]                  `gnark:",public"`
-	MessageHash emulated.Element[emparams.P256Fr]                 `gnark:",public"`
+	PublicKey   ecdsa.PublicKey[emulated.P256Fp, emulated.P256Fr] `gnark:",public"`
+	Signature   ecdsa.Signature[emulated.P256Fr]                  `gnark:",public"`
+	MessageHash emulated.Element[emulated.P256Fr]                 `gnark:",public"`
+}
+
+func VerifyP256Sig(api frontend.API, publicKey ecdsa.PublicKey[emulated.P256Fp, emulated.P256Fr], messageHash emulated.Element[emulated.P256Fr], signature ecdsa.Signature[emulated.P256Fr]) {
+	publicKey.Verify(api, sw_emulated.GetP256Params(), &messageHash, &signature)
 }
 
 func (c *P256SigVerifyCircuit) Define(api frontend.API) error {
 
-	c.PublicKey.Verify(api, sw_emulated.GetP256Params(), &c.MessageHash, &c.Signature)
+	VerifyP256Sig(api, c.PublicKey, c.MessageHash, c.Signature)
 
 	return nil
 }
@@ -35,18 +38,18 @@ func (c *P256SigVerifyCircuit) PrepareInput(input interface{}) (Circuit, []strin
 		panic("Input must be of the correct type for P256SigVerifyCircuit")
 	}
 
-	keyX := emulated.ValueOf[emparams.P256Fp](inputData.PublicKey.X.Bytes())
-	keyY := emulated.ValueOf[emparams.P256Fp](inputData.PublicKey.Y.Bytes())
-	sigR := emulated.ValueOf[emparams.P256Fr](inputData.Signature[:32])
-	sigS := emulated.ValueOf[emparams.P256Fr](inputData.Signature[32:])
-	msg := emulated.ValueOf[emparams.P256Fr](inputData.MessageHash)
+	keyX := emulated.ValueOf[emulated.P256Fp](inputData.PublicKey.X.Bytes())
+	keyY := emulated.ValueOf[emulated.P256Fp](inputData.PublicKey.Y.Bytes())
+	sigR := emulated.ValueOf[emulated.P256Fr](inputData.Signature[:32])
+	sigS := emulated.ValueOf[emulated.P256Fr](inputData.Signature[32:])
+	msg := emulated.ValueOf[emulated.P256Fr](inputData.MessageHash)
 
 	return &P256SigVerifyCircuit{
-		PublicKey: ecdsa.PublicKey[emparams.P256Fp, emparams.P256Fr]{
+		PublicKey: ecdsa.PublicKey[emulated.P256Fp, emulated.P256Fr]{
 			X: keyX,
 			Y: keyY,
 		},
-		Signature: ecdsa.Signature[emparams.P256Fr]{
+		Signature: ecdsa.Signature[emulated.P256Fr]{
 			R: sigR,
 			S: sigS,
 		},
